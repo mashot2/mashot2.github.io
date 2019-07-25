21c188ff9c6ca3a406b1090c728a2a569b6f91e3
mashot7 <mashot2@gmail.com>
2019-07-25 16:39:19 +0400

    Testing new carousel

--- js/main.js
+++ js/main.js
@@ -14,4 +14,82 @@ $(document).ready(function () {
 		preload: 0,
 		start: 2
 	});
+
+	let freeLessonBtn = $('.freeLessonBtn'),
+		myNav = $('.my-nav'),
+		myNavbar = $('.my-navbar'),
+		myNumber = $('.my-number');
+
+	if ($(document).scrollTop() > 1) {
+		if (myNav.hasClass('bg-transparent')) {
+			myNav.removeClass('bg-transparent');
+			myNav.removeClass('navbar-in-top');
+			myNav.addClass('bg-white');
+		}
+		if (myNavbar.hasClass('d-none')) {
+			myNumber.addClass('d-none');
+			myNavbar.removeClass('d-none');
+		}
+		// if (freeLessonBtn.hasClass('border-white')) {
+		// 	freeLessonBtn.removeClass('border-white');
+		// 	freeLessonBtn.removeClass('text-white');
+		// 	freeLessonBtn.addClass('border-primary');
+		// 	freeLessonBtn.addClass('text-primary');
+		// }
+	} else {
+		if (myNav.hasClass('bg-white')) {
+			myNav.addClass('navbar-in-top');
+			myNav.addClass('bg-transparent');
+			myNav.removeClass('bg-white');
+		}
+		if (myNumber.hasClass('d-none')) {
+			myNumber.removeClass('d-none');
+			myNavbar.addClass('d-none');
+		}
+		// if (freeLessonBtn.hasClass('border-primary')) {
+		// 	freeLessonBtn.removeClass('border-primary');
+		// 	freeLessonBtn.removeClass('text-primary');
+		// 	freeLessonBtn.addClass('border-white');
+		// 	freeLessonBtn.addClass('text-white');
+		// }
+	}
+
+	$(window).scroll(function () {
+		if ($(document).scrollTop() > 1) {
+			if (myNav.hasClass('bg-transparent')) {
+				myNav.removeClass('navbar-in-top');
+				myNav.removeClass('bg-transparent');
+				myNav.addClass('bg-white');
+			}
+			if (myNavbar.hasClass('d-none')) {
+				myNumber.addClass('d-none');
+				myNavbar.removeClass('d-none');
+			}
+			if (freeLessonBtn.hasClass('border-white')) {
+				freeLessonBtn.removeClass('border-white');
+				freeLessonBtn.removeClass('text-white');
+				freeLessonBtn.addClass('border-primary');
+				freeLessonBtn.addClass('text-primary');
+			}
+		} else {
+			if (myNav.hasClass('bg-white')) {
+				myNav.addClass('navbar-in-top');
+				myNav.addClass('bg-transparent');
+				myNav.removeClass('bg-white');
+			}
+			if (myNumber.hasClass('d-none')) {
+				myNumber.removeClass('d-none');
+				myNavbar.addClass('d-none');
+			}
+			if (freeLessonBtn.hasClass('border-primary')) {
+				freeLessonBtn.removeClass('border-primary');
+				freeLessonBtn.removeClass('text-primary');
+				freeLessonBtn.addClass('border-white');
+				freeLessonBtn.addClass('text-white');
+			}
+		}
+	});
 });
+
+//
+//
